f0f2e21ac350fe11d426c4a773b95b360945d61c
wramos1306 <walterm_1993@hotmail.com>
2024-12-11 18:09:17 -0300

    issue en seeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -14,23 +14,5 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-
-        User::create([
-            'name' => 'Walter Maximiliano Ramos',
-            'email' => 'licenciadowalterramos@gmail.com',
-            'password' => bcrypt('Nicolas2020')
-        ])->assignRole('Admin');
-        
-        User::create([
-            'name' => 'DR Jorge Argerich',
-            'email' => 'drjorgeargerich@gmail.com',
-            'password' => bcrypt('Morena2010')
-        ])->assignRole('Admin');
-        
-        User::create([
-            'name' => 'Dolores Gerez',
-            'email' => 'estudiodrargerich@gmail.com',
-            'password' => bcrypt('Morena2010')
-        ])->assignRole('Admin');
     }
 }
